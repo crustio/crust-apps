4c3e2e9b050bb5bc0588151dc917eae2e39c6d1d
yuhui <1161569428@qq.com>
2020-06-02 17:47:00 +0800

    change icon

--- packages/apps-config/src/ui/identityIcons/index.ts
+++ packages/apps-config/src/ui/identityIcons/index.ts
@@ -8,7 +8,7 @@ const identityNodes: Record<string, string> = [
   ['edgeware-node', 'substrate'],
   ['joystream-node', 'beachball'],
   ['node-template', 'substrate'],
-  ['crust', 'substrate'],
+  ['crust', 'polkadot'],
   ['parity-polkadot', 'polkadot'],
   ['polkadot-js', 'polkadot'],
   ['substrate-node', 'substrate']
