6d867209aed3e8a80159d28db6fb4df131638110
yuhui1208 <51014425+yuhui1208@users.noreply.github.com>
2020-07-28 12:24:06 +0800

    [App-config] add Maxwell network selection  (#31)
    * [Staking] fix change controller error (#19)
    
    * add maxwell network selection

--- packages/apps-config/src/settings/endpoints.ts
+++ packages/apps-config/src/settings/endpoints.ts
@@ -77,6 +77,11 @@ function createTest (t: <T= string> (key: string, text: string, options: { ns: s
       info: 'crust',
       text: t<string>('rpc.crust.network', 'Rocky (Crust AlphaNet, hosted by Crust Network)', { ns: 'apps-config' }),
       value: 'ws://139.196.122.228:6008/'
+    },
+    {
+      info: 'crust',
+      text: t<string>('rpc.crust.network', 'Maxwell (Crust BetaNet, hosted by Crust Network)', { ns: 'apps-config' }),
+      value: 'ws://101.132.117.183:9944/'
     }
     // {
     //   info: 'westend',
