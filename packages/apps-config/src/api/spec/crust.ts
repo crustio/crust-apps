bba33acd36cc0249d276f45256e41ecfae6dbfc1
Yashiro <51014425+yuhui1208@users.noreply.github.com>
2020-09-07 17:12:15 +0800

    [Bug] Fix account effective stake (#40)
    * [Staking] fix change controller error (#19)
    
    * fix effective stake
    
    * fix

--- packages/apps-config/src/api/spec/crust.ts
+++ packages/apps-config/src/api/spec/crust.ts
@@ -1,54 +1,56 @@
+// Copyright 2017-2020 @polkadot/apps-config authors & contributors
+// This software may be modified and distributed under the terms
+// of the Apache-2.0 license. See the LICENSE file for details.
+
+// structs need to be in order
+/* eslint-disable sort-keys */
+
 export default {
   Address: 'AccountId',
-  LookupSource: 'AccountId',
+  AddressInfo: 'Vec<u8>',
+  FileAlias: 'Vec<u8>',
+  Guarantee: {
+    targets: 'Vec<IndividualExposure<AccountId, Balance>>',
+    total: 'Compact<Balance>',
+    submitted_in: 'EraIndex',
+    suppressed: 'bool'
+  },
+  IASSig: 'Vec<u8>',
   Identity: {
     pub_key: 'Vec<u8>',
-    code: 'Vec<u8>',
+    code: 'Vec<u8>'
   },
-  SworkerCode: 'Vec<u8>',
-  // ValidatorPrefs: {
-  //   fee: 'Compact<Perbill>'
-  // },
-  // fee: 'commission',
-  WorkReport: {
-    block_number: 'u64',
-    used: 'u64',
-    reserved: 'u64',
-    cached_reserved: 'u64',
-    files: 'Vec<(Vec<u8>, u64)>',
-  },
-  StakingLedger: {
-    stash: 'AccountId',
-    total: 'Compact<Balance>',
-    active: 'Compact<Balance>',
-    unlocking: 'Vec<UnlockChunk>',
-    claimed_rewards: 'Vec<EraIndex>'
-  },
-  UnlockChunk: {
-    value:'Compact<Balance>',
-    era: 'Compact<EraIndex>'
-  },
-  Validations: {
-    total: 'Compact<Balance>',
-    guarantee_fee: 'Compact<Perbill>',
-    guarantors: 'Vec<AccountId>',
-  },
-  Nominations: {
-    targets: 'Vec<AccountId>',
-    total: 'Compact<Balance>',
-    submitted_in: 'u32',
-    suppressed: 'bool',
-  },
-  ReportSlot: 'u64',
-  AddressInfo: 'Vec<u8>',
-  MerkleRoot: 'Vec<u8>',
+  ISVBody: 'Vec<u8>',
+  LookupSource: 'AccountId',
   MerchantInfo: {
     address: 'Vec<u8>',
     storage_price: 'Balance',
-    file_map: 'Vec<(Vec<u8>, Vec<Hash>)>',
+    file_map: 'Vec<(Vec<u8>, Vec<Hash>)>'
   },
+  MerchantPunishment: {
+    success: 'EraIndex',
+    failed: 'EraIndex',
+    value: 'Balance'
+  },
+  MerkleRoot: 'Vec<u8>',
   OrderStatus: {
-    _enum: ['Success', 'Failed', 'Pending'],
+    _enum: ['Success', 'Failed', 'Pending']
+  },
+  PaymentLedger: {
+    total: 'Balance',
+    paid: 'Balance',
+    unreserved: 'Balance'
+  },
+  Pledge: {
+    total: 'Balance',
+    used: 'Balance'
+  },
+  ReportSlot: 'u64',
+  Releases: {
+    _enum: ['V1_0_0', 'V2_0_0']
+  },
+  Status: {
+    _enum: ['Free', 'Reserved']
   },
   StorageOrder: {
     file_identifier: 'Vec<u8>',
@@ -59,40 +61,17 @@ export default {
     provider: 'AccountId',
     client: 'AccountId',
     amount: 'Balance',
-    order_status: 'OrderStatus',
-  },
-  Pledge: {
-    total: 'Balance',
-    used: 'Balance',
-  },
-  // Payment ledger
-  PaymentLedger: {
-    total: 'Balance',
-    paid: 'Balance',
-    unreserved: 'Balance',
-  },
-  MerchantPunishment: {
-    success: 'EraIndex',
-    failed: 'EraIndex',
-    value: 'Balance',
+    order_status: 'OrderStatus'
   },
   SworkerCert: 'Vec<u8>',
-  IASSig: 'Vec<u8>',
-  ISVBody: 'Vec<u8>',
+  SworkerCode: 'Vec<u8>',
   SworkerPubKey: 'Vec<u8>',
   SworkerSignature: 'Vec<u8>',
-  // TODO: remove util upgrade newest polkadot-js/api
-  Releases: {
-    _enum: ['V1_0_0', 'V2_0_0'],
-  },
-  Status: {
-    _enum: ['Free', 'Reserved']
-  },
-  Guarantee: {
-    targets: 'Vec<IndividualExposure<AccountId, Balance>>',
-    total: 'Compact<Balance>',
-    submitted_in: 'EraIndex',
-    suppressed: 'bool'
-  },
-  FileAlias: 'Vec<u8>'
+  WorkReport: {
+    block_number: 'u64',
+    used: 'u64',
+    reserved: 'u64',
+    cached_reserved: 'u64',
+    files: 'Vec<(Vec<u8>, u64)>'
+  }
 };
