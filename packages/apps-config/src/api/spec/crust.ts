f97615596ecbd845c68b188c4c567a81e8c7ff9a
yuhui <1161569428@qq.com>
2020-10-20 10:09:56 +0800

    fix types

--- packages/apps-config/src/api/spec/crust.ts
+++ packages/apps-config/src/api/spec/crust.ts
@@ -6,73 +6,93 @@
 
 export default {
   Address: 'AccountId',
-    AddressInfo: 'Vec<u8>',
-    FileAlias: 'Vec<u8>',
-    Guarantee: {
-      targets: 'Vec<IndividualExposure<AccountId, Balance>>',
-      total: 'Compact<Balance>',
-      submitted_in: 'EraIndex',
-      suppressed: 'bool',
-    },
-    IASSig: 'Vec<u8>',
-    Identity: {
-      pub_key: 'Vec<u8>',
-      code: 'Vec<u8>',
-    },
-    ISVBody: 'Vec<u8>',
-    LookupSource: 'AccountId',
-    MerchantInfo: {
-      address: 'Vec<u8>',
-      storage_price: 'Balance',
-      file_map: 'Vec<(Vec<u8>, Vec<Hash>)>',
-    },
-    MerchantPunishment: {
-      success: 'EraIndex',
-      failed: 'EraIndex',
-      value: 'Balance',
-    },
-    MerkleRoot: 'Vec<u8>',
-    OrderStatus: {
-      _enum: ['Success', 'Failed', 'Pending'],
-    },
-    PaymentLedger: {
-      total: 'Balance',
-      paid: 'Balance',
-      unreserved: 'Balance',
-    },
-    Pledge: {
-      total: 'Balance',
-      used: 'Balance',
-    },
-    ReportSlot: 'u64',
-    Releases: {
-      _enum: ['V1_0_0', 'V2_0_0'],
-    },
-    Status: {
-      _enum: ['Free', 'Reserved'],
-    },
-    StorageOrder: {
-      file_identifier: 'Vec<u8>',
-      file_size: 'u64',
-      created_on: 'BlockNumber',
-      completed_on: 'BlockNumber',
-      expired_on: 'BlockNumber',
-      provider: 'AccountId',
-      client: 'AccountId',
-      amount: 'Balance',
-      order_status: 'OrderStatus',
-    },
-    SworkerCert: 'Vec<u8>',
-    SworkerCode: 'Vec<u8>',
-    SworkerPubKey: 'Vec<u8>',
-    SworkerSignature: 'Vec<u8>',
-    WorkReport: {
-      report_slot: 'u64',
-      used: 'u64', 
-      free: 'u64',
-      files:'Vec<Vec<u8>, u64>',
-      reported_files_size: 'u64',
-      reported_srd_root: 'Vec<u8>',
-      reported_files_root: 'Vec<u8>'
-    }
+  AddressInfo: 'Vec<u8>',
+  FileAlias: 'Vec<u8>',
+  Guarantee: {
+    targets: 'Vec<IndividualExposure<AccountId, Balance>>',
+    total: 'Compact<Balance>',
+    submitted_in: 'EraIndex',
+    suppressed: 'bool',
+  },
+  IASSig: 'Vec<u8>',
+  Identity: {
+    pub_key: 'Vec<u8>',
+    code: 'Vec<u8>',
+  },
+  ISVBody: 'Vec<u8>',
+  LookupSource: 'AccountId',
+  MerchantInfo: {
+    address: 'Vec<u8>',
+    storage_price: 'Balance',
+    file_map: 'Vec<(Vec<u8>, Vec<Hash>)>',
+  },
+  MerchantPunishment: {
+    success: 'EraIndex',
+    failed: 'EraIndex',
+    value: 'Balance',
+  },
+  MerkleRoot: 'Vec<u8>',
+  OrderStatus: {
+    _enum: ['Success', 'Failed', 'Pending'],
+  },
+  PaymentLedger: {
+    total: 'Balance',
+    paid: 'Balance',
+    unreserved: 'Balance',
+  },
+  Pledge: {
+    total: 'Balance',
+    used: 'Balance',
+  },
+  ReportSlot: 'u64',
+  Releases: {
+    _enum: ['V1_0_0', 'V2_0_0'],
+  },
+  SorderInfo: {
+    file_identifier: 'MerkleRoot',
+    file_size: 'u64',
+    created_on: 'BlockNumber',
+    merchant: 'AccountId',
+    client: 'AccountId',
+    amount: 'Balance',
+    duration: 'BlockNumber'
+  },
+  SorderStatus: {
+    completed_on: 'BlockNumber',
+    expired_on: 'BlockNumber',
+    status: 'OrderStatus',
+    claimed_at: 'BlockNumber'
+  },
+  SorderPunishment: {
+    success: 'BlockNumber',
+    failed: 'BlockNumber',
+    updated_at: 'BlockNumber'
+  },
+  Status: {
+    _enum: ['Free', 'Reserved'],
+  },
+  StorageOrder: {
+    file_identifier: 'Vec<u8>',
+    file_size: 'u64',
+    created_on: 'BlockNumber',
+    completed_on: 'BlockNumber',
+    expired_on: 'BlockNumber',
+    provider: 'AccountId',
+    client: 'AccountId',
+    amount: 'Balance',
+    order_status: 'OrderStatus',
+  },
+  SworkerCert: 'Vec<u8>',
+  SworkerCode: 'Vec<u8>',
+  SworkerPubKey: 'Vec<u8>',
+  SworkerSignature: 'Vec<u8>',
+  WorkReport: {
+    report_slot: 'u64',
+    used: 'u64', 
+    free: 'u64',
+    files:'BTreeMap<MerkleRoot, u64>',
+    reported_files_size: 'u64',
+    reported_srd_root: 'Vec<u8>',
+    reported_files_root: 'Vec<u8>'
+  }
 };
