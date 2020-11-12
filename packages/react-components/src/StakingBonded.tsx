64bd770bfc2d1583b8d25ac7429cf9072a394f84
yuhui <1161569428@qq.com>
2020-11-12 18:03:12 +0800

    fix

--- packages/react-components/src/StakingBonded.tsx
+++ packages/react-components/src/StakingBonded.tsx
@@ -5,7 +5,6 @@ import { DeriveStakingAccount } from '@polkadot/api-derive/types';
 
 import React from 'react';
 import { FormatBalance } from '@polkadot/react-query';
-import BN from 'bn.js';
 
 interface Props {
   className?: string;
@@ -13,12 +12,7 @@ interface Props {
 }
 
 function StakingBonded ({ className = '', stakingInfo }: Props): React.ReactElement<Props> | null {
-
-  const stakingLedger = stakingInfo && JSON.parse(JSON.stringify(stakingInfo));
-  let balance = new BN(0);
-  if (stakingInfo && stakingLedger) {
-    balance = new BN((Number(stakingLedger?.active)).toString());
-  }
+  const balance = stakingInfo?.stakingLedger?.active.unwrap();
 
   if (!balance?.gtn(0)) {
     return null;
