e51772dc2bb4cbde6354267b2ddb6e8b550b6c98
zhangxu1161 <zhangxu1161@gmail.com>
2020-12-15 15:40:48 +0800

    fix pined tag not render

--- packages/apps-ipfs/src/files/files-list/FilesList.js
+++ packages/apps-ipfs/src/files/files-list/FilesList.js
@@ -118,6 +118,7 @@ export class FilesList extends React.Component {
   }
 
   componentDidUpdate () {
+    console.log(this.props.pins);
     const selected = this.state.selected.filter((name) => (
       this.props.files.find((el) => el.name === name)
     ));
@@ -287,41 +288,6 @@ export class FilesList extends React.Component {
     );
   }
 
-  rowRenderer = ({ index, key, style }) => {
-    const { canDrop, files, filesPathInfo, isOver, onAddFiles, onInspect, onNavigate, pins, contracts } = this.props;
-    const { focused, isDragging, selected } = this.state;
-
-    const pinsString = pins.map((p) => p.toString());
-    const contractString = contracts.map((p) => p.toString());
-
-    return (
-      <div key={key}
-        ref={(r) => { this.filesRefs[files[index].name] = r; }}
-        style={style}>
-        <File
-          {...files[index]}
-          focused={focused === files[index].name}
-          handleContextMenuClick={this.props.handleContextMenuClick}
-          isMfs={filesPathInfo.isMfs}
-          name={files[index].name}
-          onAddFiles={onAddFiles}
-          onMove={this.move}
-          onNavigate={() => {
-            if (files[index].type === 'unknown') {
-              onInspect(files[index].cid);
-            } else {
-              onNavigate(files[index].path);
-            }
-          }}
-          onSelect={this.toggleOne}
-          pinned={pinsString.includes(files[index].cid.toString())}
-          contracted={contractString.includes(files[index].cid.toString())}
-          selected={selected.indexOf(files[index].name) !== -1}
-          setIsDragging={this.isDragging}
-          translucent={isDragging || (isOver && canDrop)} />
-      </div>
-    );
-  }
 
   onRowsRendered = ({ startIndex }) => this.setState({ firstVisibleRow: startIndex })
 
@@ -338,45 +304,48 @@ export class FilesList extends React.Component {
     className = `FilesList no-select sans-serif border-box w-100 flex flex-column ${className}`;
 
     return connectDropTarget(
-      <section className={className}
-        ref={(el) => { this.root = el; }}>
-        { showLoadingAnimation
-          ? <LoadingAnimation />
-          : <Fragment>
-            <header className='gray pv3 flex items-center flex-none'
-              style={{ paddingRight: '1px', paddingLeft: '1px' }}>
+      <section
+        className={className}
+        ref={el => {
+          this.root = el;
+        }}
+      >
+        {showLoadingAnimation ? (
+          <LoadingAnimation />
+        ) : (
+          <Fragment>
+            <header
+              className="gray pv3 flex items-center flex-none"
+              style={{ paddingRight: "1px", paddingLeft: "1px" }}
+            >
               <div className={checkBoxCls}>
-                <Checkbox aria-label={t('selectAllEntries')}
-                  checked={allSelected}
-                  onChange={this.toggleAll}/>
+                <Checkbox aria-label={t("selectAllEntries")} checked={allSelected} onChange={this.toggleAll} />
               </div>
-              <div className='ph2 f6 flex-auto' style={{ textAlign: 'left' }}>
-                <button aria-label={ t('sortBy', { name: t('app:terms.name') })}
-                  onClick={this.changeSort(sorts.BY_NAME)}>
-                  {t('app:terms.name')} {this.sortByIcon(sorts.BY_NAME)}
+              <div className="ph2 f6 flex-auto" style={{ textAlign: "left" }}>
+                <button
+                  aria-label={t("sortBy", { name: t("app:terms.name") })}
+                  onClick={this.changeSort(sorts.BY_NAME)}
+                >
+                  {t("app:terms.name")} {this.sortByIcon(sorts.BY_NAME)}
                 </button>
               </div>
-              <div className='ph2 pv1 flex-none dn db-l tr mw3'>
-                { /* Badges */ }
-              </div>
-              <div className='pl2 pr4 tr f6 flex-none dn db-l mw4 w-10'>
-                <button aria-label={ t('sortBy', { name: t('size') })}
-                  onClick={this.changeSort(sorts.BY_SIZE)}>
-                  {t('app:terms.size')} {this.sortByIcon(sorts.BY_SIZE)}
+              <div className="ph2 pv1 flex-none dn db-l tr mw3">{/* Badges */}</div>
+              <div className="pl2 pr4 tr f6 flex-none dn db-l mw4 w-10">
+                <button aria-label={t("sortBy", { name: t("size") })} onClick={this.changeSort(sorts.BY_SIZE)}>
+                  {t("app:terms.size")} {this.sortByIcon(sorts.BY_SIZE)}
                 </button>
               </div>
-              <div className='pa2'
-                style={{ width: '2.5rem' }} />
+              <div className="pa2" style={{ width: "2.5rem" }} />
             </header>
             <WindowScroller>
               {({ height, isScrolling, onChildScroll, scrollTop }) => (
-                <div className='flex-auto'>
+                <div className="flex-auto">
                   <AutoSizer disableHeight>
                     {({ width }) => (
                       <List
-                        aria-label={ t('filesListLabel')}
+                        aria-label={t("filesListLabel")}
                         autoHeight
-                        className='outline-0'
+                        className="outline-0"
                         data={files /* NOTE: this is a placebo prop to force the list to re-render */}
                         height={height}
                         isScrolling={isScrolling}
@@ -386,16 +355,65 @@ export class FilesList extends React.Component {
                         ref={this.listRef}
                         rowCount={rowCount}
                         rowHeight={55}
-                        rowRenderer={this.rowRenderer}
+                        rowRenderer={({ index, key, style }) => {
+                          const {
+                            canDrop,
+                            files,
+                            filesPathInfo,
+                            isOver,
+                            onAddFiles,
+                            onInspect,
+                            onNavigate,
+                            pins,
+                            contracts
+                          } = this.props;
+                          const { focused, isDragging, selected } = this.state;
+                          const pinsString = pins.map(p => p.toString());
+                          const contractString = contracts.map(p => p.toString());
+                          return (
+                            <div
+                              key={key}
+                              ref={r => {
+                                this.filesRefs[files[index].name] = r;
+                              }}
+                              style={style}
+                            >
+                              <File
+                                {...files[index]}
+                                focused={focused === files[index].name}
+                                handleContextMenuClick={this.props.handleContextMenuClick}
+                                isMfs={filesPathInfo.isMfs}
+                                name={files[index].name}
+                                onAddFiles={onAddFiles}
+                                onMove={this.move}
+                                onNavigate={() => {
+                                  if (files[index].type === "unknown") {
+                                    onInspect(files[index].cid);
+                                  } else {
+                                    onNavigate(files[index].path);
+                                  }
+                                }}
+                                onSelect={this.toggleOne}
+                                pinned={pinsString.includes(files[index].cid.toString())}
+                                contracted={contractString.includes(files[index].cid.toString())}
+                                selected={selected.indexOf(files[index].name) !== -1}
+                                setIsDragging={this.isDragging}
+                                translucent={isDragging || (isOver && canDrop)}
+                              />
+                            </div>
+                          );
+                        }}
                         scrollTop={scrollTop}
-                        width={width} />
+                        width={width}
+                      />
                     )}
                   </AutoSizer>
                 </div>
               )}
             </WindowScroller>
             {this.selectedMenu}
-          </Fragment> }
+          </Fragment>
+        )}
       </section>
     );
   }
