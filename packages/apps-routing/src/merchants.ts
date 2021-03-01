8ffec539c4fa20b1c5d0bacb17e4fd3320e13120
yuhui <1161569428@qq.com>
2021-03-01 14:16:31 +0800

    fix ci

--- packages/apps-routing/src/merchants.ts
+++ packages/apps-routing/src/merchants.ts
@@ -4,7 +4,7 @@
 import type { TFunction } from 'i18next';
 import type { Route } from './types';
 
-import Component from '@polkadot/app-merchants';
+import Component from '@polkadot/apps-merchants/';
 
 export default function create (t: TFunction): Route {
   return {
