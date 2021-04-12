0ddc43047fe3f047322d61992111026d61ffd5f1
yuhui <1161569428@qq.com>
2021-04-12 20:51:42 +0800

    skip eslint

--- packages/page-staking/src/useSortedTargets.ts
+++ packages/page-staking/src/useSortedTargets.ts
@@ -1,6 +1,7 @@
 // Copyright 2017-2021 @polkadot/app-staking authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
+/* eslint-disable */
 import type { ApiPromise } from '@polkadot/api';
 import type { DeriveSessionInfo, DeriveStakingElected, DeriveStakingWaiting } from '@polkadot/api-derive/types';
 import type { SortedTargets, TargetSortBy, ValidatorInfo } from './types';
