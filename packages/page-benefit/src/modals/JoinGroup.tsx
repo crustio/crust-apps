25ad6675486cea558ef5d04b43593eb4eb84c3cd
Yashiro <1161569428@qq.com>
2021-08-24 17:40:47 +0800

    Fix join group

--- packages/page-benefit/src/modals/JoinGroup.tsx
+++ packages/page-benefit/src/modals/JoinGroup.tsx
@@ -1,11 +1,11 @@
 // Copyright 2017-2021 @polkadot/app-accounts authors & contributors
 // SPDX-License-Identifier: Apache-2.0
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 
 import { useTranslation } from '@polkadot/apps/translate';
-import { InputAddress, Modal, TxButton } from '@polkadot/react-components';
+import { InputAddress, MarkError, Modal, TxButton } from '@polkadot/react-components';
 import { useApi } from '@polkadot/react-hooks';
 
 interface Props {
@@ -20,6 +20,25 @@ function JoinGroup ({ className = '', onClose, recipientId: propRecipientId, sen
   const { api } = useApi();
   const [senderId, setSenderId] = useState<string | null>(propSenderId || null);
   const [recipientId, setRecipientId] = useState<string | null>(propRecipientId || null);
+  const [isDisable, setIsDisable] = useState<boolean>(true);
+
+  useEffect(() => {
+    const address = JSON.parse(JSON.stringify(senderId));
+    if (address) {  
+      api.query.swork
+      .identities<any>(address)
+      .then((identity): void => {
+        const identityOpt = JSON.parse(JSON.stringify(identity));
+        if (identityOpt) {
+          setIsDisable(false)
+        } else {
+          setIsDisable(true)
+        }
+      })
+      .catch((): void => setIsDisable(true));
+    }
+
+  }, [api, senderId])
 
   return (
     <Modal
@@ -39,8 +58,10 @@ function JoinGroup ({ className = '', onClose, recipientId: propRecipientId, sen
                 onChange={setSenderId}
                 type='account'
               />
+              {isDisable && (<MarkError content={t<string>('Please wait for member account to report the first work report and then join the group')} />)}
             </Modal.Columns>
           </Modal.Content>
+
           <Modal.Content>
             <Modal.Columns hint={t<string>('The beneficiary will have access to the transferred fees when the transaction is included in a block.')}>
               <InputAddress
@@ -60,6 +81,7 @@ function JoinGroup ({ className = '', onClose, recipientId: propRecipientId, sen
           accountId={senderId}
           icon='paper-plane'
           label={t<string>('Join group')}
+          isDisabled={isDisable}
           onStart={onClose}
           params={[recipientId]}
           tx={api.tx.swork.joinGroup}
