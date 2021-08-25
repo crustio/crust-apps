d1383d433143b8414262bd1c246dae5c47e83397
eericxu <2681350846@qq.com>
2021-08-25 11:28:11 +0800

    add config ipfs-endpoints

---
+++ packages/apps-config/src/ipfs-endpoints/index.ts
@@ -0,0 +1,10 @@
+// Copyright 2017-2021 @polkadot/apps-config authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import type { AuthIpfsEndpoint } from './types';
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export function createAuthIpfsEndpoints (t: Function): AuthIpfsEndpoint[] {
+  return [
+  ];
+}
