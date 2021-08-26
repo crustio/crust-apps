2406e81c0719968a5c1cb68eff2aecc26377f81b
Yashiro <1161569428@qq.com>
2021-08-26 13:48:00 +0800

    Fix

--- packages/react-api/src/config.ts
+++ packages/react-api/src/config.ts
@@ -6,13 +6,12 @@ import { EthereumNetworkOptions } from './lib/configuration';
 export const ethereums: Record<string, Record<number, EthereumNetworkOptions>> = {
   Crust: {
     1: {
-      bridge: '0xC84456ecA286194A201F844993C220150Cf22C63',
-      erc20: '0x6c5ba91642f10282b576d91922ae6448c9d52f4e',
-      erc20AssetHandler: '0x6eD3bc069Cf4F87DE05c04C352E8356492EC6eFE',
-      erc20ResourceId:
-            '0x00000000000000000000000000000063a7e2be78898ba83824b0c0cc8dfb6001',
+      bridge: '0x0964A01E0d0B5d6FF726Ab9D60a93d188D3f505B',
+      erc20: '0x32a7C02e79c4ea1008dD6564b35F131428673c41',
+      erc20AssetHandler: '0x9D332427e6D1B91d9cf8d2fa3B41Df2012887aAB',
+      erc20ResourceId: '0x000000000000000000000000000000608d1bc9a2d146ebc94667c336721b2801',
       peerChainIds: {
-        Crust: 1
+        'Crust': 1
       }
     }
   },
@@ -21,8 +20,7 @@ export const ethereums: Record<string, Record<number, EthereumNetworkOptions>> =
       bridge: '0x9B82DAF85E9dcC4409ed13970035a181fB411542',
       erc20: '0x002f24009df0c1e9215c98cec76f18d8eaf3db0f',
       erc20AssetHandler: '0x30BdFa99ddAe21a1Ee83e213Ff29121381DDEa54',
-      erc20ResourceId:
-          '0x000000000000000000000000000000608d1bc9a2d146ebc94667c336721b2801',
+      erc20ResourceId: '0x000000000000000000000000000000608d1bc9a2d146ebc94667c336721b2801',
       peerChainIds: {
         'Crust Rocky': 1
       }
@@ -30,11 +28,10 @@ export const ethereums: Record<string, Record<number, EthereumNetworkOptions>> =
   },
   'Crust Maxwell': {
     1: {
-      bridge: '0xC84456ecA286194A201F844993C220150Cf22C63',
-      erc20: '0x6c5ba91642f10282b576d91922ae6448c9d52f4e',
-      erc20AssetHandler: '0x6eD3bc069Cf4F87DE05c04C352E8356492EC6eFE',
-      erc20ResourceId:
-            '0x00000000000000000000000000000063a7e2be78898ba83824b0c0cc8dfb6001',
+      bridge: '0x0964A01E0d0B5d6FF726Ab9D60a93d188D3f505B',
+      erc20: '0x32a7C02e79c4ea1008dD6564b35F131428673c41',
+      erc20AssetHandler: '0x9D332427e6D1B91d9cf8d2fa3B41Df2012887aAB',
+      erc20ResourceId: '0x000000000000000000000000000000608d1bc9a2d146ebc94667c336721b2801',
       peerChainIds: {
         'Crust Maxwell': 1
       }
