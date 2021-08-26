7fe567a09e9d474719a0288e34ab4c6cacdd23ca
Zikun Fan <zikunvan@gmail.com>
2021-08-26 17:28:31 +0800

    Change text and add lint ignore

--- packages/apps-config/src/ipfsGWEndpoints/gateways.ts
+++ packages/apps-config/src/ipfsGWEndpoints/gateways.ts
@@ -1,21 +1,22 @@
-// Copyright 2017-2021 @polkadot/ui-settings authors & contributors
-// SPDX-License-Identifier: Apache-2.0
+// Copyright 2019-2021 Crust Network authors & contributors
+// License: Apache-2.0
 
 import type { TFunction } from 'i18next';
 import type { Option } from '../settings/types';
 
 // Definitions here are with the following values -
 //   info: the name of a logo as defined in ../ui/logos, specifically in namedLogos
-//   text: The text you wish to display in the dropdown
-//   value: The actual gateway value
+//   text: the IPFS endpoint name
+//   value: the IPFS endpoint domain
+//   location: IPFS gateway location
 
 export function createIpfsGatewayEndpoints (t: TFunction): Option[] {
   return [
     {
       info: 'crust', // 'default' or null
-      text: t('Gateway by Crust Network'),
-      value: 'https://xxx.crust.network',
-      location: t('some where')
+      text: t('Crust Network'),
+      value: 'https://crustwebsites.net',
+      location: t('Singapore')
     }
   ];
 }
--- packages/apps-config/src/ipfsGWEndpoints/index.ts
+++ packages/apps-config/src/ipfsGWEndpoints/index.ts
@@ -1,4 +1,4 @@
-// Copyright 2017-2021 @polkadot/apps-config authors & contributors
-// SPDX-License-Identifier: Apache-2.0
+// Copyright 2019-2021 Crust Network authors & contributors
+// License: Apache-2.0
 
 export * from './gateways';
