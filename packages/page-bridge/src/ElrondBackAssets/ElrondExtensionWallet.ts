2fb53b57d2d17c45bf57a7bee78adc10418ab870
Yashiro <51014425+yashirooooo@users.noreply.github.com>
2022-09-28 15:29:07 +0800

    Fix elrond nonce (#365)
    * Fix elrond nonce
    
    * Fix lint

--- packages/page-bridge/src/ElrondBackAssets/ElrondExtensionWallet.ts
+++ packages/page-bridge/src/ElrondBackAssets/ElrondExtensionWallet.ts
@@ -12,17 +12,24 @@ const ElrondBridgePoolAddress = 'erd1drg6ndpqv3wvn0pu90al2magq7cwzar72sa6x0aws9w
 export class ElrondExtensionWallet {
   isInit = false;
   provider: ExtensionProvider;
+  address = '';
   constructor () {
     this.provider = ExtensionProvider.getInstance();
   }
 
   async signTransactions (data: any) {
     try {
-      await this.provider.init();
-      await this.provider.login();
+      if (this.address.length === 0) {
+        await this.provider.init();
+        this.address = await this.provider.login();
+      }
+
+      const addressOfAccount = new Address(this.address);
+      const accountOnNetwork = await networkProvider.getAccount(addressOfAccount);
 
       const tx = new Transaction({
         value: '0',
+        nonce: accountOnNetwork.nonce,
         receiver: new Address(ElrondBridgePoolAddress),
         gasLimit: 500000,
         data: new TransactionPayload(data),
