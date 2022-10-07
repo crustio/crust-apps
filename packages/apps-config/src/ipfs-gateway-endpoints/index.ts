791db060af78f6a26ee423f8e3888c43935ed7dd
Yashiro <51014425+yashirooooo@users.noreply.github.com>
2022-10-07 12:09:50 +0800

    disable shanghai gateway (#366)

--- packages/apps-config/src/ipfs-gateway-endpoints/index.ts
+++ packages/apps-config/src/ipfs-gateway-endpoints/index.ts
@@ -33,11 +33,11 @@ export function createAuthIpfsEndpoints (t: TFunction): AuthIpfsEndpoint[] {
       text: t('Crust Network'),
       value: 'https://ipfs-gw.decloud.foundation'
     },
-    {
-      location: t('Shanghai'),
-      text: t('️⚡ Thunder Gateway'),
-      value: 'https://gw.crustapps.net'
-    },
+    // {
+    //   location: t('Shanghai'),
+    //   text: t('️⚡ Thunder Gateway'),
+    //   value: 'https://gw.crustapps.net'
+    // },
     // {
     //   location: t('Beijing'),
     //   text: t('️Deklod'),
