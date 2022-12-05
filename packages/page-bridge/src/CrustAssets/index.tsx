83bdb7af07ff266dc2ddbe48de66844f508d7d6f
Yashiro <1161569428@qq.com>
2022-12-05 14:23:07 +0800

    Add bridge limit

--- packages/page-bridge/src/CrustAssets/index.tsx
+++ packages/page-bridge/src/CrustAssets/index.tsx
@@ -8,11 +8,12 @@ import styled from 'styled-components';
 
 import { useTranslation } from '@polkadot/apps/translate';
 import { Button, Card, Columar, Input, InputAddress, InputBalance, MarkWarning, TxButton } from '@polkadot/react-components';
-import { BN_ZERO } from '@polkadot/util';
+import { BN_ZERO, formatBalance } from '@polkadot/util';
 import logoCrust from '../images/crust.svg';
-import Banner from '@polkadot/app-accounts/Accounts/Banner';
 import { Keyring } from '@polkadot/api';
 import { useMainnetApi } from '@polkadot/react-hooks/useMainnetApi';
+import { useApi, useCall } from '@polkadot/react-hooks';
+import type { Balance } from '@polkadot/types/interfaces';
 
 interface Props {
   className?: string;
@@ -31,21 +32,29 @@ const getMainnetAddr = (addr: string) => {
 
 function CrustAssets ({ className = '', senderId: propSenderId }: Props): React.ReactElement<Props> {
   const { t } = useTranslation();
-  const { api } = useMainnetApi();
+  const { api: mainnetApi } = useMainnetApi();
+  const { api } = useApi();
   const [amount, setAmount] = useState<BN | undefined>(BN_ZERO);
   const [senderId, setSenderId] = useState<string | null>(propSenderId || null);
   const [receiveId, setReceiveId] = useState<string | null>('' || null);
   const [isAmountError, setIsAmountError] = useState<boolean>(true);
   const [isValid, setIsValid] = useState(false);
+  const bridgeLimit = useCall<Balance>(api.query.bridgeTransfer.bridgeLimit);
+
+  console.log('bridgeLimit::', bridgeLimit)
 
   useEffect(() => {
-
-    if (Number(amount) <= 0) {
-      setIsAmountError(true)
-    } else {
-      setIsAmountError(false)
+    if (bridgeLimit) {
+      console.log('bridgeLimit::', formatBalance(bridgeLimit, { decimals: 12, forceUnit: '-', withUnit: true }))
+      console.log('amount::', formatBalance(amount, { decimals: 12, forceUnit: '-', withUnit: true }))
+      if (amount && Number(amount) <= 0 && bridgeLimit.gt(amount)) {
+        setIsAmountError(true)
+      } else {
+        setIsAmountError(false)
+      }
     }
-  }, [amount])
+
+  }, [amount, bridgeLimit])
 
   const onChangeShadowAddress = useCallback((address: string) => {
     const isValidShadowAddr = getMainnetAddr(address);
@@ -62,9 +71,6 @@ function CrustAssets ({ className = '', senderId: propSenderId }: Props): React.
     <Columar>
       <Columar.Column>
         <Card withBottomMargin>
-          <Banner type='warning'>
-            <p>{t<string>('This function is an internal test stage, the assets will not be lost, but there may be a delay (max to 48 hours) in the arrival of the account.')}&nbsp;<a target="_blank" href={'https://shadow-apps.crust.network/?rpc=wss%3A%2F%2Frpc-shadow.crust.network#/accounts'}>{t<string>('You can check the CSM assets here...')}</a></p>
-          </Banner>
           <h3><span style={{ fontWeight: 'bold' }}>{t<string>('From Crust')}</span></h3>
           <div style={{ display: 'flex' }}>
             <img src={logoCrust as string}
@@ -93,7 +99,7 @@ function CrustAssets ({ className = '', senderId: propSenderId }: Props): React.
                     isError={!isValid}
                     label={t<string>('Crust parachain address')}
                     onChange={onChangeShadowAddress}
-                    placeholder={t<string>('cT prefixed address')}
+                    placeholder={t<string>('es prefixed address')}
                     value={receiveId || ''}
                 />
             </div>
@@ -110,7 +116,11 @@ function CrustAssets ({ className = '', senderId: propSenderId }: Props): React.
                 onChange={setAmount}
                 withMax
               />
-              <MarkWarning content={t<string>('Please reserve a small amount of CRU as transaction fee')}>
+              <MarkWarning content={t<string>(`Please make sure that the amount you want to transfer is less than the the transfer bridge limit ({{limit}}).`, {
+                replace: {
+                  limit: formatBalance(bridgeLimit, { decimals: 12, forceUnit: '-', withUnit: true })
+                }
+              })}>
               </MarkWarning>
             </div>
           </div>
@@ -121,18 +131,11 @@ function CrustAssets ({ className = '', senderId: propSenderId }: Props): React.
               isDisabled={ isAmountError }
               label={t<string>('Transfer')}
               params={[amount, receiveId]}
-              tx={api.tx.bridgeTransfer.transferToPolkadotParachain}
+              tx={mainnetApi.tx.bridgeTransfer.transferToPolkadotParachain}
             />
           </Button.Group>
         </Card>
       </Columar.Column>
-      {/* <Columar.Column>
-        <Card>
-          <Banner type="warning">
-            <p>{t<string>('Cross-chain transfers are automatically executed by smart contracts. after the execution of the contract is completed, the funds will arrive in the account. Please wait patiently.')}&nbsp;<a target="_blank" href={bridgeTxStatusLink}>{t<string>('You can check the transaction status here...')}</a></p>
-          </Banner>
-        </Card>
-      </Columar.Column> */}
     </Columar>
   </div>);
 }
