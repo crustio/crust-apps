7e4236bad38047fe50927140d86b6bd16a361d22
Smallwolf2021 <82216270+Smallwolf2021@users.noreply.github.com>
2023-05-24 17:14:16 +0800

    Update index.ts
    Remove no validate node

--- packages/apps-config/src/ipfs-gateway-endpoints/index.ts
+++ packages/apps-config/src/ipfs-gateway-endpoints/index.ts
@@ -56,14 +56,6 @@ export function createAuthIpfsEndpoints (t: TFunction): AuthIpfsEndpoint[] {
       status: 'online'
       // crustAddress: cTL7AwVGvYfTvXuEX61HbPGLiqaf2P7RwHNiJJargDL46dz1Z
       // TG:@mstflint
-    },
-    {
-      location: t('河南联通'),
-      text: t('️Crust IPFS GW'),
-      value: 'https://gw.w3ipfs.net:7443',
-      status: 'online'
-      // crustAddress: cTL7AwVGvYfTvXuEX61HbPGLiqaf2P7RwHNiJJargDL46dz1Z
-      // TG:@mstflint
     }
     // {
     //   location: t('Beijing'),
