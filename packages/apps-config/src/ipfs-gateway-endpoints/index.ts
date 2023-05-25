11fe44373c02b73bfaf9600d2cb8fd7f44b9b94c
LowEntropyBody <jszyyx@163.com>
2023-05-25 16:05:47 +0800

    Update index.ts

--- packages/apps-config/src/ipfs-gateway-endpoints/index.ts
+++ packages/apps-config/src/ipfs-gateway-endpoints/index.ts
@@ -14,12 +14,12 @@ export function createAuthIpfsEndpoints (t: TFunction): AuthIpfsEndpoint[] {
   return [
     // for prod
     {
-    value: 'https://gateway.aitimeout.site:18001',
+    value: 'http://gateway.aitimeout.site:18001',
     text: t('hangzhou'),
     location: t('Hangzhou'),
     // crustAddress: cTHrHT5ro6a55cu2FxmmrtJDBy3GMUK86tGC1yfmu4nvqkS63
     // contactInformation: tele: btc eth or email: eoseth59@gmail.com
-    status: 'online'
+    status: 'inactivated'
     },
     {
       location: t('Singapore'),
